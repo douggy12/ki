0864d6518ccf0bca4dd42782cc61c284c412126a
Geoffroy Guittet <gguittet@sodifrance.fr>
2019-02-22 16:51:48 +0100

    Amélioration de l'affichage général du front

--- kifront/src/app/component/team-detail/team-detail.component.ts
+++ kifront/src/app/component/team-detail/team-detail.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Location } from '@angular/common';
 import { ContextService } from '../../service/Context.service';
+import { HostListener } from "@angular/core";
 declare var $: any;
 
 @Component({
@@ -17,26 +18,55 @@ export class TeamDetailComponent implements OnInit, OnChanges {
   @Input() team: Team;
   myId: number;
   isPilote: boolean;
+  expanded: boolean = false;
+  tDescription : String;
+  scrHeight:any;
+  sidebarHeight:any;
+  wrapperHeight:any;
+  contentHeight:any;
+  
+  @HostListener('window:resize', ['$event'])
+    getScreenSize(event?) {
+      this.scrHeight = window.innerHeight;
+    }
 
   constructor(
     // private route: ActivatedRoute,
     // private location: Location
     private auth: AuthService,
     private context: ContextService
-  ) { }
+  ) {
+    this.getScreenSize();
+   }
 
   ngOnInit(): void {
     // this.route.paramMap
     // .switchMap((params: ParamMap) => this.teamService.getTeam(+params.get('id')))
-    // .subscribe(team => this.team = team) ;
-
+    // .subscribe(team => this.team = team);
   }
+  
   ngOnChanges(): void {
     this.isPilote = this.auth.isPilote(this.context.me, this.team);
     $(() => {
         $('.content-header').height($('.info-equipe').height() + 1);
-    });
+        this.sidebarHeight = $('.content-wrapper').height() + $('.sidebar-menu').height() ;
+        $('.sidebar').height(this.sidebarHeight);
+        $('.sidebar').css('max-height', this.sidebarHeight);
+    }); 
   }
 
+  resetExpanded(): void{
+    this.expanded = !this.expanded;
+  }
 
+  setTDescription(text: String): String {
+    if(this.expanded){
+      return this.team.kiTeam.description;
+    }else if (text != null){
+      return this.team.kiTeam.description.split('</p>')[0] + '..';
+    }else{
+      return '';
+    }
+  }
 }
+
